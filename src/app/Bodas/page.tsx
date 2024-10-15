6184814c02ccffcf5878afe5cb821ae03a920e22
duskalor <duskalor1@gmail.com>
2024-10-15 08:07:20 -0500

    tnak

--- src/app/Bodas/page.tsx
+++ src/app/Bodas/page.tsx
@@ -3,24 +3,23 @@ import Link from "next/link";
 
 import {getBoda} from "@/lib/api.boda";
 import {buttonVariants} from "@/components/ui/button";
+import {BodaList} from "@/components/bodaList";
 
 const page = async () => {
-  const boda = await getBoda();
+  const bodas = await getBoda();
 
   return (
     <section className="w-full">
-      <div>
+      <div className="flex w-full justify-between px-5">
         <h1 className="text-2xl font-bold text-gray-800 dark:text-white">Bodas</h1>
         <Link className={buttonVariants({variant: "secondary"})} href="/Bodas/create">
           New boda
         </Link>
       </div>
 
-      {boda.map((body) => (
-        <Link key={body.id} className="px-5 text-right" href={`/Guest/${body.id}`}>
-          {body.name}
-        </Link>
-      ))}
+      <div className="flex w-full flex-col gap-10">
+        <BodaList bodas={bodas} />
+      </div>
     </section>
   );
 };
---
+++ src/components/bodaList.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import {Boda} from "@prisma/client";
+import Link from "next/link";
+
+import {
+  TableCaption,
+  TableHeader,
+  TableRow,
+  TableHead,
+  TableBody,
+  TableCell,
+  TableFooter,
+  Table,
+} from "./ui/table";
+import {Button} from "./ui/button";
+
+export function BodaList({bodas}: {bodas: Boda[]}) {
+  return (
+    <Table className="w-1/2">
+      <TableCaption>A list of your recent bodas.</TableCaption>
+      <TableHeader>
+        <TableRow className="[&>th]:cursor-pointer [&>th]:select-none">
+          <TableHead>NAMES</TableHead>
+          <TableHead>CREATE AT</TableHead>
+          <TableHead className="text-right">ACCIONES</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {bodas.map((boda) => (
+          <TableRow key={boda.id}>
+            <TableCell className="font-medium">
+              <Link key={boda.id} className="" href={`/Guest/${boda.id}`}>
+                {boda.name}
+              </Link>
+            </TableCell>
+            <TableCell>{boda.createAt.toLocaleDateString("es-ES")}</TableCell>
+            <TableCell className="flex justify-between gap-2">
+              <Button>Edit</Button>
+              <Button>Delete</Button>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+      <TableFooter>
+        <TableRow>
+          <TableCell colSpan={2}>Total</TableCell>
+          <TableCell className="text-right">S/{bodas.length}</TableCell>
+        </TableRow>
+      </TableFooter>
+    </Table>
+  );
+}
